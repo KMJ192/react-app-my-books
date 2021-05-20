6ec910406f14130ba376910c08f64c39b998ed73
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-05-20 20:36:51 +0900

    books-sample

---
+++ README.md
@@ -0,0 +1,6 @@
+# React 책장 앱 만들기 강의 저장용
+
+### 사용 라이브러리
+ - React : cra (typescript)
+ - react-router-dom
+ - react-error-boundary
---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Edit from './pages/Edit';
+import Detail from './pages/Detail';
+import Add from './pages/Add';
+import Signin from './pages/Signin';
+import NotFound from './pages/NotFound';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/edit/:id" component={Edit}/>
+        <Route exact path="/book/:id" component={Detail}/>
+        <Route exact path="/add" component={Add}/>
+        <Route exact path="/signin" component={Signin}/>
+        <Route exact path="/" component={Home}/>
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
